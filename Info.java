a071730b53d44aa0d5db01a6a4bc73c9b0c56f0d
QIU Mingming <qiumingming7@gmail.com>
2017-06-02 17:35:21 +0200

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,15 @@
+package com.androidtutorialpoint.googlemapsdistancecalculator;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Administrator on 2017/5/31 0031.
+ */
+
+public class Info extends FragmentActivity{
+    protected void onCreat(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
